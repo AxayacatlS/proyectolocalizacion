f67e48f4d33271786ab242dc7122e5775671adec
AxayacatlS <38516115+AxayacatlS@users.noreply.github.com>
2021-05-18 23:27:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         look-at="[gps-camera]"
         scale="50 50 50"
       ></a-text>
-	  <a-entity gltf-model="concha.glb" scale="2 2 2" position="0 0.5 -3" rotation="0 0 0"></a-entity>
+	  <a-entity gltf-model="concha.glb" look-at="[gps-camera]" scale="15 15 15" position="0 0.5 -3" rotation="0 0 0"></a-entity>
       <a-camera gps-camera rotation-reader> </a-camera>
     </a-scene>
   </body>
